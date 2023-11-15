f093a77521ddbe38f5809aedf910d2200bbcfc4f
drozer <wwwhotring53@gmail.com>
2023-11-15 17:58:18 +0000

    Fix some issues

--- main.cpp
+++ main.cpp
@@ -8,21 +8,22 @@
 #define JOURNALD_LOGGING
 
 #ifndef JOURNALD_LOGGING
-    extern "C" {
-        #include "log.c/log.h"
-    }
+extern "C"
+{
+#include "log.c/log.h"
+}
 
-    #define log_t(...) log_trace(__LINE__, __VA_ARGS__)
-    #define log_e(...) log_e(__LINE__, __VA_ARGS__)
-    #define log_w(...) log_warning(__LINE__, __VA_ARGS__)
-    #define log_i(...) log_info(__LINE__, __VA_ARGS__)
+#define log_t(...) log_trace(__LINE__, __VA_ARGS__)
+#define log_e(...) log_e(__LINE__, __VA_ARGS__)
+#define log_w(...) log_warning(__LINE__, __VA_ARGS__)
+#define log_i(...) log_info(__LINE__, __VA_ARGS__)
 #else
-    #include <systemd/sd-journal.h>
+#include <systemd/sd-journal.h>
 
-    #define log_t(...) sd_journal_print(1, __VA_ARGS__)
-    #define log_e(...) sd_journal_print(3, __VA_ARGS__)
-    #define log_w(...) sd_journal_print(4, __VA_ARGS__)
-    #define log_i(...) sd_journal_print(6, __VA_ARGS__)
+#define log_t(...) sd_journal_print(1, __VA_ARGS__)
+#define log_e(...) sd_journal_print(3, __VA_ARGS__)
+#define log_w(...) sd_journal_print(4, __VA_ARGS__)
+#define log_i(...) sd_journal_print(6, __VA_ARGS__)
 #endif
 
 char ERR_BUF[PCAP_ERRBUF_SIZE];
@@ -34,8 +35,6 @@ void handler(u_char *user, const struct pcap_pkthdr *h,
              const u_char *bytes);
 int main(int, char **)
 {
-    setuid(0);
-    system("id");
     if (pcap_init(PCAP_CHAR_ENC_UTF_8, ERR_BUF) != 0)
     {
         log_e(ERR_BUF);
@@ -51,17 +50,17 @@ int main(int, char **)
 
     do
     {
-        if (strcmp(device_list->name, "enp1s0") == 0)
+        if (strcmp(device_list->name, "ens33") == 0)
         {
             break;
         }
-
-        if (device_list == NULL)
-        {
-            log_e("No interface enp1s0");
-            return -1;
-        }
     } while (device_list = device_list->next);
+
+    if (device_list == NULL)
+    {
+        log_e("No interface enp1s0");
+        return -1;
+    }
 
     pcap_t *capture = pcap_create(device_list->name, ERR_BUF);
 
@@ -80,12 +79,13 @@ int main(int, char **)
         pcap_close(capture);
         return -1;
     }
-    
-    if (pcap_set_immediate_mode(capture, 1) != 0) {
+
+    if (pcap_set_immediate_mode(capture, 1) != 0)
+    {
         log_e(pcap_geterr(capture));
         pcap_close(capture);
         return -1;
-    } 
+    }
 
     if (pcap_activate(capture) != 0)
     {
@@ -94,14 +94,14 @@ int main(int, char **)
         return -1;
     }
 
-    pcap_dumper_t* f_dumper = pcap_dump_open(capture, "capture.pcap");
+    pcap_dumper_t *f_dumper = pcap_dump_open(capture, "capture.pcap");
 
     while (true)
     {
         pcap_pkthdr *p_head;
         const u_char *p_data;
 
-        pcap_dispatch(capture, 1, (process_packet)handler, (u_char*)f_dumper);
+        pcap_dispatch(capture, 1, (process_packet)handler, (u_char *)f_dumper);
     }
 }
 
@@ -113,14 +113,15 @@ void handler(u_char *user, const struct pcap_pkthdr *h,
     const struct sniff_ethernet *ethernet; /* The ethernet header */
     const struct sniff_ip *ip;             /* The IP header */
     const struct sniff_tcp *tcp;           /* The TCP header */
-    const u_char *payload;                   /* Packet payload */
+    const u_char *payload;                 /* Packet payload */
 
     u_int size_ip;
     u_int size_tcp;
 
     ethernet = (struct sniff_ethernet *)(packet);
 
-    if (ethernet->ether_type != IP_TYPE) {
+    if (ethernet->ether_type != IP_TYPE)
+    {
         log_w("Only IP packets can be handled 0x%08x\n", ethernet->ether_type);
         return;
     }
@@ -147,6 +148,10 @@ void handler(u_char *user, const struct pcap_pkthdr *h,
     char src_str[INET_ADDRSTRLEN];
     char dst_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(ip->ip_src), src_str, INET_ADDRSTRLEN);
-    inet_ntop(AF_INET, &( ip->ip_dst), dst_str, INET_ADDRSTRLEN);
-    log_i("Packet captured From: %s To: %s", src_str, dst_str);
+    inet_ntop(AF_INET, &(ip->ip_dst), dst_str, INET_ADDRSTRLEN);
+
+    if (!((strcmp(src_str, "192.168.203.1") == 0) || (strcmp(dst_str, "192.168.203.1") == 0)))
+    {
+        log_i("Packet captured From: %s To: %s", src_str, dst_str);
+    }
 }
